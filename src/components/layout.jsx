21dd266d6b829dcfe15e5bc39bf937f0d4713d7d
dimdimah <dimahluthfi15@gmail.com>
2024-01-13 12:47:37 +0700

    feat: tambah footer komponen

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,14 @@
+import Footer from "./footer";
+
 /* eslint-disable react/prop-types */
 export default function Layout({ children }) {
   return (
     <div className="w-full h-screen overflow-auto bg-white text-neutral-800">
       {/* {taruh navbar di sini} */}
-      <div className="container px-8 py-4 mx-auto grow">{children}</div>
+      <div className="container px-8 py-4 mx-auto grow">
+        {children}
+        <Footer />
+      </div>
     </div>
   );
 }
